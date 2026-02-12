3854a637677366526aac83414547b5563aff4fd9
Aurelien <pr61gsu@eduvaud.ch>
2026-02-12 16:32:45 +0100

    Code update

---
+++ code/game_over.py
@@ -0,0 +1,34 @@
+# Auteur: Aurélien Nicollas
+# Date: 12/02/2026
+# Fonction: Afficher un écran de fin
+
+# IMPORTS
+
+import random
+from time import sleep
+from tkinter import *
+from tkinter.font import *
+
+# FENÊTRE
+
+root = Tk()
+root.geometry("600x600")
+root.title("Jeu du 2048 - Menu")
+font = Font(family="Century Gothic", size=70, weight=BOLD)
+btn_font = Font(family="Century Gothic", size=50, weight=NORMAL)
+
+# FONCTIONS
+
+def play():
+    root.destroy()
+    import gfx
+
+title = Label(root, text="Game over !", font=font, fg=("#ff5757"))
+title.pack(pady=80)
+
+btn_play = Button(root, text="Jouer !", bg=("#8ad3f2"), fg="white", font=btn_font, width=14, command=play)
+btn_play.pack(side=BOTTOM, pady=100)
+
+# DÉMARRAGE
+
+root.mainloop()
--- code/main.py
+++ code/main.py
@@ -2,5 +2,14 @@
 # Date: 12/02/2026
 # Fonction: Compiler tout les programmes
 
-# IMPORTS
-import menu
+# CECI EST TEMPORAIRE
+# CE CODE SERT UNIQUEMENT À LA VÉRIFICATION DE MON TRAVAIL
+
+print("""Que souhaitez-vous voir ? :
+[1] Menu
+[2] Gameover""")
+temp = input("1 ou 2 : ")
+if temp == "1":
+    import menu
+else:
+    import game_over
