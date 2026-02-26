5b4e509dfcb9747efb961af77dcd4873bc1fe406
Aurelien <pr61gsu@eduvaud.ch>
2026-02-26 16:39:58 +0100

    Updating rules

--- code/rules.py
+++ code/rules.py
@@ -15,28 +15,17 @@ def fusion(a, b, c, d):
             a, b, c, d = b, c, d, 0
             cpt += 1
     if a == b and a > 0:
-        a = a * 2
-        b = 0
+        a,b,c,d = a * 2, c, d, 0
         cpt += 1
     if b == c and b > 0:
-        b = b * 2
-        c = 0
+        b, c, d = b * 2, d, 0
         cpt += 1
     if c == d and c > 0:
-        c = c * 2
-        d = 0
+        c, d = c * 2, 0
         cpt += 1
-    if 0 in (a, b, c):
-        if c == 0:
-            c, d = d, 0
-            cpt += 1
-        if b == 0:
-            b, c, d = c, d, 0
-            cpt += 1
-        if a == 0:
-            a, b, c, d = b, c, d, 0
-            cpt += 1
+
     print(a, b, c, d, "Effectué en", cpt, "coups")
+    return [a, b, c, d]
 
 
 fusion(0,0,0,2)
@@ -44,5 +33,3 @@ fusion(0, 0, 2, 2)
 fusion(2, 0, 2, 2)
 fusion(2, 2, 2, 2)
 fusion(2, 2, 4, 0)
-
-
